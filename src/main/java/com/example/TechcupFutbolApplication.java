9e8a1d414d11180b3fc198462ab5d3e5b2139b80
Lanch3ros <jose.lancheros-a@mail.escuelaing.edu.co>
2026-03-05 15:25:51 -0500

    Initial commit: Estructura del proyecto basada en Lab 5

---
+++ src/main/java/com/example/TechcupFutbolApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcupFutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechcupFutbolApplication.class, args);
+	}
+
+}

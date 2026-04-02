e7306473267e05d329a10a42efff71b049b15e92
Lanch3ros <jose.lancheros-a@mail.escuelaing.edu.co>
2026-04-01 20:47:40 -0500

    docs: implement JWT SecurityScheme in Swagger/OpenAPI config

--- src/main/java/com/example/config/SwaggerConfig.java
+++ src/main/java/com/example/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.example.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +13,18 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String schemeName = "bearerAuth";
         return new OpenAPI()
                 .info(new Info()
                         .title("API de TechCup Fútbol")
                         .version("1.0")
-                        .description("Documentación de los endpoints REST para la plataforma digital de gestión del torneo semestral de fútbol TechCup."));
+                        .description("Documentación de los endpoints REST para la plataforma digital de gestión del torneo semestral de fútbol TechCup."))
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components()
+                        .addSecuritySchemes(schemeName, new SecurityScheme()
+                                .name(schemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
